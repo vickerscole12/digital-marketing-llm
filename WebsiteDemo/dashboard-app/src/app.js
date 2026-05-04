9ba99cd88cc65f352c57c88dcfc478d5f7c3d344
vickerscole12 <114261063+vickerscole12@users.noreply.github.com>
2026-05-04 18:30:24 -0400

    Add files via upload

---
+++ WebsiteDemo/dashboard-app/src/app.js
@@ -0,0 +1,7 @@
+/* const app = () => {
+    // This function can be used to initialize any dynamic content or interactivity in the future
+    console.log("Dashboard App Initialized");
+};
+
+// Call the app function to initialize the application
+app();*/
